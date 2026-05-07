4704a12739c5554998598f89d4ae9bbc7985c105
Gourav prakash <gouravmehta9905@gmail.com>
2026-05-07 23:11:21 +0530

    Add Netlify config for frontend subfolder build

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FDh3bJT9j4pOtJUTqXnQ6qUO4N2twVvJWVG+vih3yLM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9HgdNV8qclrRUrkRg/nzgXgS5T3RqtJP1aiuPhKq+bs=\"\n}"
